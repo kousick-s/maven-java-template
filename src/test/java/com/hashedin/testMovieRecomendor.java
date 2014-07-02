6c4d5da7a9c2e0dba5eaa6b232a26d7d69802da2
kousick <kousick.s@hashedin.com>
2014-07-02 19:31:32 +0530

    Changed few Naming Conventions

--- src/test/java/com/hashedin/testMovieRecomendor.java
+++ src/test/java/com/hashedin/testMovieRecomendor.java
@@ -9,16 +9,26 @@ import junit.framework.Assert;
 public class testMovieRecomendor {
 	
 	public static void main(String[] args){
-		Movie mov=new Movie();
+		MovieManager mov=new MovieManager();
 		ArrayList<MovieRatings> rathmap=new ArrayList<MovieRatings>();
 		Map<String,MovieRecomendor> movhmap = new HashMap<String, MovieRecomendor>();
-		movhmap=mov.getMovies("test_movie.data");
+		movhmap=mov.getMovies("movie.data");
 		//Map<String,MovieRatings> rathmap = new HashMap<String, MovieRatings>();
-		rathmap=mov.getRatings("test_ratings.data");
+		rathmap=mov.getRatings("ratings.data");
 		Map<String,MovieUser> userhmap = new HashMap<String, MovieUser>();
-		userhmap=mov.getUsers("test_user.data");
-		String MovieName=mov.getMovieByGenre(rathmap, movhmap, mov,"test_movie.data");
-		System.out.println(MovieName);
+		userhmap=mov.getUsers("user.data");
+		String MovieNamebyYear=mov.getMovieByYear(rathmap, movhmap, mov,"movie.data");
+		System.out.println("Movie by Year -"+MovieNamebyYear);
+		String MovieNamebyYearGenre=mov.getMovieByYearGenre(rathmap, movhmap, mov,"movie.data");
+		System.out.println("Movie by Year and Genre -"+MovieNamebyYearGenre);
+		String MovieNamebyGenre=mov.getMovieByGenre(rathmap, movhmap, mov,"movie.data");
+		System.out.println("Movie by Genre -"+MovieNamebyGenre);
+		System.out.println("Most Active User -");
+		mov.getMostActiveUser(rathmap, userhmap);
+		String MostWatchedGenres=mov.getMostWatchedGenre(mov, rathmap, movhmap);
+		System.out.println(" Most Watched Genres: " + MostWatchedGenres);
+		String HighestRatedGenres=mov.getHighestRatedGenre(mov,rathmap, movhmap);
+		System.out.println("Highest Rated Genres:"+HighestRatedGenres);
 		
 	}
 
